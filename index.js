e68754674a8595b0b9895352f918554e63ec55e6
TJ Alston <tjalston82@yahoo.com>
2016-08-06 19:46:44 -0400

    Still having issues with code to match cards.

--- index.js
+++ index.js
@@ -2,98 +2,87 @@ const handleImgClick = () => {
   console.log('BANANA!!!')
 }
 let clicked = true
+let divClicked = []
+let arrayOfClicked = Array(36)
 
-const init = () => {
-
-  for (let i = 0; i < 36; i++){
-  const img = `url('bananas.jpg')`
-  const images = document.querySelector('.images')
-  const div = document.createElement('div')
-  div.className = 'img ${i}'
-  div.style.backgroundImage = img
-  div.addEventListener('click', () => {
-    console.log('BANANA')
-    if (clicked){
-      div.style.backgroundImage = `url('./minion${i + 1}.jpg')`
-      clicked = false
-    } else {
-      div.style.backgroundImage = `url ('bananas.jpg')`
-      clicked = true
-    }
-})
-  images.appendChild(div)
-}
-}
-document.addEventListener('DOMContentLoaded', init)
-
-
-// const init = () => {
-//   let cellOne = document.querySelector('td')
-//
-//   cellOne.addEventListener('click', () => {
-//       console.log('BELLO!')
-//   })
+// const handleClickEvent = () => {
+//   let div = event.target
+//   divClicked.push(div.className)
+//   if (clicked) {
+//   div.style.backgroundImage =
+//   }
 // }
-// //   let cellOne = document.querySelector(".min1")
-// //   cellOne.addEventListener('dblclick', () => {
-// //     if (firstClick){
-// //       cellOne.style.backgroundImage = "url('bananas.jpg')"
+const img = "url('bananas.jpg')"
+const bgImage = "url('./minion${i + 1}.jpg')"
+const bkImage = "url('./images1/minion${i + 1}).jpg)"
+// intailizes the entire card function.
+const init = () => {
+  // loops the cards to turn back and forth between images and 2nd image folder.(2)
+  for (let j = 0; j < 2; j++) {
+    // loops the cards to turn back and forth within each individual image folder
+    for (let i = 0; i < 18; i++) {
+      const images = document.querySelector('.images')
+      const div = document.createElement('div')
+      div.className = `img ${i} hidden`
+      div.style.backgroundImage = img
+  // addEventListener tells the computer when you hear this "click" do this function
+      div.addEventListener('click', () => {
+        let pic = event.target
+        // console.log('BANANA')
+        if (clicked) {
+      // if clicked calls the div in order to style the background image to a minion, the ${i + 1} means that it will continue down the numeric image list to complete the grid of pictures.
+          div.style.backgroundImage = `url('./images1/minion${i + 1}.jpg')`
+          //  if clicked the statement is no longer true and will show a minion
+          clicked = false
+          divClicked.push(pic.style.backgroundImage)
+          console.log(pic.style.backgroundImage)
+          console.log(divClicked)
+        } else {
+          div.style.backgroundImage = "url('bananas.jpg')"
+          //  if the not clicked it will remain true.
+          clicked = true
+        }
+      })
+      // will take the images and append it to the end of this fomula
+      images.appendChild(div)
+    }
+  }
+    var clicks = '0' // counts how may picks have been made in each turn
+    var firstchoice // stores index of first card selected
+    var secondchoice // stores index of second card selected
 
-//
-// // let array = [
-// // ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D'],
-// // ['E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'],
-// // ['I', 'I', 'J', 'J', 'K', 'K', 'L', 'L'],
-// // ['M', 'M', 'N', 'N', 'O', 'O', 'P', 'P']
-// // ]
-// // let array = [
-// //   ['0', '0', '0', '0', '0', '0', '0', '0',],
-// //   ['0', '0', '0', '0', '0', '0', '0', '0',],
-// //   ['0', '0', '0', '0', '0', '0', '0', '0',],
-// //   ['0', '0', '0', '0', '0', '0', '0', '0',]
-// // ]
-// // let firstClick = true
-// // const cellValues = {0: "url('back3.jpg')"}
-// // const init = () =>{
-// //
-// //   // const moment = require('moment')
-// //   // console.log(moment().format());
-// //   // moment().format('MMMM Do YYYY, h:mm:ss a');
-// //
-// //   let cellOne = document.querySelector(".min1")
-// //   cellOne.addEventListener('dblclick', () => {
-// //     if (firstClick){
-// //       cellOne.style.backgroundImage = "url('bananas.jpg')"
-// //       firstClick = false
-// //     }else{
-// //       cellOne.style.backgroundImage = "url('minion1.jpg')"
-// //       firstClick = true
-// //     }
-// //     console.log('Bananas!')
-// //   })
-// //
-// //   let cellTwo = document.querySelector(".min2")
-// //   cellTwo.addEventListener('dblclick', () => {
-// //     console.log('working')
-// //     clicked = false
-// //   })
-// //
-// //   const init = () => {
-// //   const rows = document.querySelectorAll('tr')
-// //   // loop through the table rows
-// //   for (let i = 0; i < rows.length; i++) {
-// //     // loop through the table data (cells)
-// //     const cols = rows[i].querySelectorAll('td')
-// //     for (let j = 0; j < cols.length; j++) {
-// //       // establishes that we can click on the table data in the current cell.
-// //       cols[j].addEventListener('dblclick', (disable) => {
-// //         // draws x or o in each cell and changes the player's turn
-// //         play(i, j, playerTurn)
-// //         drawBoard()
-// //       })
-// //     }
-// //   }
-// // }
-// // }
-//
-// document.addEventListener('DOMContentLoaded', init)
+    var match = '0' // counts matches made
+    var backcard = 'bananas.jpg' // shows back of card when turned over
+
+    function choose (card) {
+       if (clicks == 2) {
+           return;
+       }
+       if (clicks == 0) {
+           firstchoice = card;
+           document.images[card].src = bgImage[card]
+           clicks = 1;
+       } else {
+           clicks = 2;
+           secondchoice = card;
+           document.images[card].src = bkImage[card]
+           timer = setInterval("check()", 1000)
+       }
+   }
+   /* Check to see if a match is made */
+
+function check() {
+   clearInterval(timer); //stop timer
+   if (bgImage[secondchoice] == bkImage[firstchoice]) {
+       match++;
+       document.getElementById('matches').innerHTML = match
+   } else {
+       document.images[firstchoice].src = backcard
+       document.images[secondchoice].src = backcard
+       clicks = 0
+       return
+   }
+}
+}
+
+document.addEventListener('DOMContentLoaded', init)
